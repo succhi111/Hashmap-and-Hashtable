71a17bcbd72745327f9a537ce759b9347850575d
succhi111 <suchendramishra396@gmail.com>
2022-12-07 16:10:55 +0530

    Initial commit

---
+++ src/com/bridgelabz/HashTableMain.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+
+public class HashTableMain {
+    public static void main(String[] args) {
+
+        MyHashTable<String, Integer> hashTable = new MyHashTable();
+        String sentence = "To be or not to be";
+
+        String[] sentenceArray = sentence.toLowerCase().split(" ");
+
+        for (String word : sentenceArray) {
+            Integer value = hashTable.get(word);
+
+
+            if (value == null)
+                value = 1;
+            else
+                value = value + 1;
+            hashTable.add(word, value);
+        }
+        System.out.println(hashTable);
+    }
+}
